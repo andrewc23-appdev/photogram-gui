a21de47b2908fea72dbbb74f62d5993f9c15de00
andrewchen23 <andrewc23@uchicago.edu>
2023-02-16 07:53:07 +0000

    photo v3 still missing comment add

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -43,4 +43,41 @@ class PhotosController < ApplicationController
     #render({:template => "photo_templates/create.html.erb"})
   end
 
+  def update
+
+    the_id = params.fetch("modify_id")
+    the_photo = Photo.where({:id => the_id}).first
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    redirect_to("/photos/" + the_photo.id.to_s)
+    #render({:template => "photo_templates/update.html.erb"})
+
+  end
+
+
+  def comment_new
+    #add_comment?query_photo_id=953&query_auhtor_id=81&query_comment=adfasdf
+    input_photo_id = params.fetch("query_photo_id")
+    input_author_id = params.fetch("query_auhtor_id")
+    input_comment = params.fetch("query_comment")
+
+    a_new_comment = Comment.new
+
+    a_new_comment.photo_id = input_photo_id
+    a_new_comment.author_id = input_author_id
+    a_new_comment.body = input_comment
+
+    a_new_comment.save
+
+    redirect_to("/photos/" + a_new_comment.photo_id.to_s)
+  end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+
+    #Parameters: {"query_image"=>"asdf", "query_caption"=>"asdf", "query_owner_id"=>"asdf"}
+    input_username = params.fetch("input_username")
+
+
+    a_new_user = User.new
+
+    a_new_user.username = input_username
+
+    a_new_user.save
+
+    redirect_to("/users/" + a_new_user.username.to_s)
+
+    #render({:template => "photo_templates/create.html.erb"})
+  end
+
+  def update
+
+    the_id = params.fetch("modify_id")
+    the_user = User.where({:id => the_id}).first
+
+    input_username = params.fetch("input_username")
+
+    the_user.username = input_username
+
+
+    the_user.save
+
+    redirect_to("/users/" + the_user.username.to_s)
+    #render({:template => "photo_templates/update.html.erb"})
+
+  end
+
 end
