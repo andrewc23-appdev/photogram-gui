3d46eb1f3252a4a6944935e237aee521c6e94933
andrewchen23 <andrewc23@uchicago.edu>
2023-02-16 06:58:33 +0000

    photo v2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get("/users/:path_username", {:controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index"})
-  get("/photos/:path_photos", { :controller => "photos", :action => "show"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
+
+  get("/insert_photo", { :controller => "photos", :action => "create"})
 
 end
